306ec7ed8cef3fd6361beb1f0ef981f74bf03a39
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-12-17 21:25:35 +0800

    作業完成90%

---
+++ UnityProBuilderAnimation/Assets/Script/Coin.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+ 
+    public GameObject coo;
+
+
+    public void coin()
+    {
+        Instantiate(coo, new Vector3(0, 0.8f, 0), Quaternion.identity);
+    }
+    
+
+   
+
+
+
+}
+
+
+
