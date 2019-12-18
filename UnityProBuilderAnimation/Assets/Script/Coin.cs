03d706166ced8fc0905f62277ddef6876128aec1
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-12-18 19:00:58 +0800

    作業完成

--- UnityProBuilderAnimation/Assets/Script/Coin.cs
+++ UnityProBuilderAnimation/Assets/Script/Coin.cs
@@ -5,11 +5,13 @@ public class Coin : MonoBehaviour
 {
  
     public GameObject coo;
-
+    public AudioSource cw;
+    public AudioClip sw;
 
     public void coin()
     {
         Instantiate(coo, new Vector3(0, 0.8f, 0), Quaternion.identity);
+        cw.PlayOneShot(sw);
     }
     
 
